0855416b92ec6b523ab73b5a89468017695b6ea3
Sagar G V <sagar.writeme@gmail.com>
2013-07-05 16:40:46 +0530

    LICENSE stamp

--- tb.v
+++ tb.v
@@ -1,26 +1,22 @@
 `timescale 1ns / 1ps
+/*
+    uartrx receives data on coded in UART
+    Copyright (C) 2013  Sagar G V
+    E-mail : sagar.writeme@gmail.com
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   15:01:13 07/05/2013
-// Design Name:   uartrx
-// Module Name:   K:/fpga_projs/uart_rx/tb.v
-// Project Name:  uart_rx
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: uartrx
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*///////////////////////////////////////////////////////////////
 
 module tb;
 
